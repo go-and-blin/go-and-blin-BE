5bff7f0c93bb22cfad85ad519ec09d6f61b40349
kang-sky <yallo428@gmail.com>
2025-01-04 05:52:08 +0900

    [#8] Test: 카테고리 수정 기능 테스트 - CategoryServiceTest update() 테스트 작성 - CategoryControllerTest update() 테스트 작성 - CategoryControllerTest update() newTitle 공백에 대한 검증 테스트 작성

--- src/test/java/com/goblin/goandblinblog/domain/category/controller/CategoryControllerTest.java
+++ src/test/java/com/goblin/goandblinblog/domain/category/controller/CategoryControllerTest.java
@@ -1,10 +1,14 @@
 package com.goblin.goandblinblog.domain.category.controller;
 
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.goblin.goandblinblog.ControllerTestSupport;
-import com.goblin.goandblinblog.domain.category.controller.dto.CategoryCreateRequest;
+import com.goblin.goandblinblog.domain.category.controller.dto.request.CategoryCreateRequest;
+import com.goblin.goandblinblog.domain.category.controller.dto.request.CategoryUpdateRequest;
 import com.goblin.goandblinblog.domain.category.entity.CategoryType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -22,5 +26,34 @@ class CategoryControllerTest extends ControllerTestSupport {
                         .content(objectMapper.writeValueAsString(request))
                         .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(status().isCreated());
+    }
+
+    @DisplayName("카테고리를 수정한다.")
+    @Test
+    void updateCategory() throws Exception {
+        Long categoryId = 1L;
+        CategoryUpdateRequest categoryUpdateRequest = new CategoryUpdateRequest("Test");
+
+        doNothing().when(categoryService).update(categoryId, categoryUpdateRequest.toServiceRequest());
+
+        mockMvc.perform(
+                patch("/api/v1/categories/" + categoryId)
+                        .content(objectMapper.writeValueAsString(categoryUpdateRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNoContent());
+    }
+
+    @DisplayName("카테고리를 수정할 때, 새로운 타이틀값은 필수값이다.")
+    @Test
+    void updateCategoryWhenEmptyTitle() throws Exception {
+        Long categoryId = 1L;
+        CategoryUpdateRequest categoryUpdateRequest = new CategoryUpdateRequest("");
+
+        mockMvc.perform(
+                patch("/api/v1/categories/" + categoryId)
+                        .content(objectMapper.writeValueAsString(categoryUpdateRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.validation.newTitle").value("새 카테고리 이름을 입력해주세요."));
     }
 }
--- src/test/java/com/goblin/goandblinblog/domain/category/service/CategoryServiceTest.java
+++ src/test/java/com/goblin/goandblinblog/domain/category/service/CategoryServiceTest.java
@@ -8,10 +8,12 @@ import com.goblin.goandblinblog.IntegrationTestSupport;
 import com.goblin.goandblinblog.domain.category.controller.port.CategoryService;
 import com.goblin.goandblinblog.domain.category.entity.Category;
 import com.goblin.goandblinblog.domain.category.entity.CategoryType;
+import com.goblin.goandblinblog.domain.category.service.dto.CategoryUpdateServiceRequest;
 import com.goblin.goandblinblog.domain.category.service.dto.request.CategoryCreateServiceRequest;
 import com.goblin.goandblinblog.domain.category.service.port.CategoryRepository;
 import com.goblin.goandblinblog.global.exception.ErrorCode;
 import com.goblin.goandblinblog.global.exception.category.CategoryExistsException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,8 +30,8 @@ class CategoryServiceTest extends IntegrationTestSupport {
     @Test
     void createCategory() {
         CategoryCreateServiceRequest request = new CategoryCreateServiceRequest(
-            CategoryType.ALL,
-            "트러블 슈팅"
+                CategoryType.ALL,
+                "트러블 슈팅"
         );
 
         assertDoesNotThrow(() -> {
@@ -39,21 +41,39 @@ class CategoryServiceTest extends IntegrationTestSupport {
 
     @DisplayName("카테고리 생성 할 때 이미 존재한다면 예외가 발생한다.")
     @Test
-    void createCategoryWithExistingCategory(){
+    void createCategoryWithExistingCategory() {
         Category category1 = Category.create(CategoryType.ALL, "트러블 슈팅");
         categoryRepository.save(category1);
 
         CategoryCreateServiceRequest request = new CategoryCreateServiceRequest(
-            CategoryType.ALL,
-            "트러블 슈팅"
+                CategoryType.ALL,
+                "트러블 슈팅"
         );
 
         CategoryExistsException exception = assertThrows(
-            CategoryExistsException.class, () -> {
-                categoryService.create(request);
-            });
+                CategoryExistsException.class, () -> {
+                    categoryService.create(request);
+                });
 
         assertEquals(ErrorCode.CATEGORY_EXISTS.getMessage(), exception.getErrorCode().getMessage());
+    }
+
+    @DisplayName("카테고리를 수정한다.")
+    @Test
+    void updateCategory() {
+        Category category1 = Category.create(CategoryType.ALL, "트러블슈팅");
+        Category save = categoryRepository.save(category1);
+        Long categoryId = save.getId();
+        CategoryUpdateServiceRequest request = new CategoryUpdateServiceRequest("HTTP");
+
+        categoryService.update(categoryId, request);
+
+        Category result = categoryRepository.findById(categoryId);
+
+        Assertions.assertThat(result).extracting(
+                "id", "type", "title"
+        ).contains(categoryId, CategoryType.ALL, "HTTP");
+
     }
 
 
