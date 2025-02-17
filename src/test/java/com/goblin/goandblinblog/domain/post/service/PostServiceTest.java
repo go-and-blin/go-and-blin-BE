f911b2242de97ee1aabe0304832786835f17370e
kang-sky <yallo428@gmail.com>
2025-02-17 17:20:39 +0900

    [#19] Test: Post 수정 기능 테스트 - 정상적인 데이터 요청 시 글 수정 성공 테스트 - 존재하지 않는 id 요청 시 PostNotFoundException 발생 테스트

--- src/test/java/com/goblin/goandblinblog/domain/post/service/PostServiceTest.java
+++ src/test/java/com/goblin/goandblinblog/domain/post/service/PostServiceTest.java
@@ -1,5 +1,8 @@
 package com.goblin.goandblinblog.domain.post.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import com.goblin.goandblinblog.IntegrationTestSupport;
 import com.goblin.goandblinblog.domain.category.entity.Category;
 import com.goblin.goandblinblog.domain.category.entity.CategoryType;
@@ -7,10 +10,12 @@ import com.goblin.goandblinblog.domain.category.service.port.CategoryRepository;
 import com.goblin.goandblinblog.domain.member.entity.Member;
 import com.goblin.goandblinblog.domain.member.service.port.MemberRepository;
 import com.goblin.goandblinblog.domain.post.controller.port.PostService;
+import com.goblin.goandblinblog.domain.post.entity.Post;
 import com.goblin.goandblinblog.domain.post.service.dto.request.PostCreateServiceRequest;
+import com.goblin.goandblinblog.domain.post.service.dto.request.PostUpdateServiceRequest;
 import com.goblin.goandblinblog.domain.post.service.port.PostRepository;
+import com.goblin.goandblinblog.global.exception.post.PostNotFoundException;
 import java.util.UUID;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -31,19 +36,19 @@ class PostServiceTest extends IntegrationTestSupport {
     @Autowired
     private MemberRepository memberRepository;
 
-    Long memberId = 1L;
-    Long categoryId = 1L;
+    Member member = null;
+    Category category = null;
 
     @BeforeEach
     void setUp() {
-        categoryId = categoryRepository.save(Category.create(CategoryType.GO, "스프링 부트")).getId();
-        memberId = memberRepository.save(
+        category = categoryRepository.save(Category.create(CategoryType.GO, "스프링 부트"));
+        member = memberRepository.save(
                 Member.builder()
                         .nickName("test")
                         .password("test")
                         .imageUrl("test")
                         .build()
-        ).getId();
+        );
     }
 
     @AfterEach
@@ -57,12 +62,49 @@ class PostServiceTest extends IntegrationTestSupport {
     @Test
     void createPost() {
         String uuid = String.valueOf(UUID.randomUUID());
+        PostCreateServiceRequest request = createPostCreateRequest(uuid);
 
-        PostCreateServiceRequest request = new PostCreateServiceRequest(uuid, "test", "test", categoryId);
+        String id = postService.create(member.getId(), request);
 
-        String id = postService.create(memberId, request);
+        assertThat(id).isEqualTo(uuid);
 
-        Assertions.assertThat(id).isEqualTo(uuid);
+    }
 
+    @DisplayName("글을 수정한다")
+    @Test
+    void updatePost() {
+        String uuid = String.valueOf(UUID.randomUUID());
+        PostCreateServiceRequest request = createPostCreateRequest(uuid);
+        postRepository.save(Post.create(request.uuid(), request.title(), request.content(), member, category));
+
+        PostUpdateServiceRequest updateRequest = createUpdateRequest();
+
+        String id = postService.update(uuid, updateRequest);
+        Post result = postRepository.findById(id);
+
+        assertThat(result).extracting(
+                "title", "content"
+        ).contains(updateRequest.title(), updateRequest.content());
+    }
+
+    @DisplayName("수정하려는 글이 존재하지 않는다면, PostNotFound 발생한다")
+    @Test
+    void updatePostWithPostNotFound() {
+        String uuid = String.valueOf(UUID.randomUUID());
+
+        assertThatThrownBy(
+                () -> postService.update(uuid, createUpdateRequest()))
+                .isInstanceOf(PostNotFoundException.class);
+    }
+
+    private PostUpdateServiceRequest createUpdateRequest() {
+        return new PostUpdateServiceRequest(
+                "update title",
+                "update content"
+        );
+    }
+
+    private PostCreateServiceRequest createPostCreateRequest(String uuid) {
+        return new PostCreateServiceRequest(uuid, "test", "test", category.getId());
     }
 }
