c22d057dbdb84196f34bc87d3aac47653c941ff2
sky <yallo428@gmail.com>
2025-02-25 14:27:18 +0900

    [Feat] Post 상세 조회 기능 구현 (#32)
    * [#22] Feat: Post 상세 조회 기능 구현
    
    * [#22] Test: Post 상세 조회 기능 테스트

--- src/test/java/com/goblin/goandblinblog/domain/post/controller/PostControllerTest.java
+++ src/test/java/com/goblin/goandblinblog/domain/post/controller/PostControllerTest.java
@@ -1,11 +1,14 @@
 package com.goblin.goandblinblog.domain.post.controller;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.goblin.goandblinblog.ControllerTestSupport;
+import com.goblin.goandblinblog.domain.post.service.dto.response.PostInfoResponse;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -13,6 +16,22 @@ import org.springframework.http.MediaType;
 class PostControllerTest extends ControllerTestSupport {
 
     private final String POST_API = "/api/v1/posts";
+
+    @DisplayName("단일 글을 조회한다.")
+    @Test
+    void getPost() throws Exception{
+
+        String postId = "1";
+        PostInfoResponse postInfoResponse = new PostInfoResponse(postId, "test", "test", "test", LocalDateTime.now());
+
+        when(postService.findById(postId)).thenReturn(postInfoResponse);
+
+        mockMvc.perform(
+                get(POST_API + "/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(print())
+                .andExpect(status().isOk());
+    }
 
     @DisplayName("전체 글을 조회한다.")
     @Test
