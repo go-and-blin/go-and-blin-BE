69796801e9745a955f836406760eda4654af7b93
kang-sky <yallo428@gmail.com>
2025-02-25 17:28:58 +0900

    [#34] Feat: Post Update 및 Delete 예외처리 추가 - Member compareTo를 추가하여 id 검증 로직 구현 - Post validateMember에서 compareTo 로직을 사용하여 멤버 체크 후 접근 권한이 없을 경우 Exception 발생

--- src/main/java/com/goblin/goandblinblog/domain/post/entity/Post.java
+++ src/main/java/com/goblin/goandblinblog/domain/post/entity/Post.java
@@ -6,8 +6,6 @@ import com.goblin.goandblinblog.global.domain.base.BaseTimeEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -15,6 +13,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -64,8 +63,15 @@ public class Post extends BaseTimeEntity {
         this.category = category;
     }
 
-    public void update(String title, String content) {
+    public void update(Long memberId, String title, String content) {
+        validateMember(memberId);
         this.title = title;
         this.content = content;
+    }
+
+    public void validateMember(Long memberId) {
+        if(!member.compareTo(memberId)){
+            throw new AccessDeniedException(String.valueOf(memberId));
+        }
     }
 }
